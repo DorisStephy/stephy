c91adfa7902d0a673c4b299e68ea9afd13aacc2b
DorisStephy <47077231+DorisStephy@users.noreply.github.com>
2019-02-06 22:22:40 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -6,6 +6,6 @@ while temp!=0:
   rev=(rev*0)+(temp%10)
   temp=temp//10
 if temp==rev:
-  print("the number is palindrome")
+  print("yes")
 else:
-  print("the number is not palindrome")
+  print("no")
